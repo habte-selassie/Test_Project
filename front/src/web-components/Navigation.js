7b64a5783dc1c388d6d041a414a43588edf54ec0
Habte Selassie <habtuchelsea1000@gmail.com>
2023-06-23 13:27:25 +0300

    update commit

--- front/src/web-components/Navigation.js
+++ front/src/web-components/Navigation.js
@@ -2,7 +2,7 @@ import * as React from 'react';
 import Box from '@mui/material/Box';
 import BottomNavigation from '@mui/material/BottomNavigation';
 import BottomNavigationAction from '@mui/material/BottomNavigationAction';
-import RestoreIcon from '@mui/icons-material/Restore';
+import RestoreIcon from '@mui/material/';
 import FavoriteIcon from '@mui/icons-material/Favorite';
 import LocationOnIcon from '@mui/icons-material/LocationOn';
 import { useState } from 'react';
