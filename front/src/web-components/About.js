eba60af5f637b72d7bb5c7a31014c0adad3e3a8f
Habte Selassie <habtuchelsea1000@gmail.com>
2023-06-23 14:46:44 +0300

    add jsx code to advertisment

--- front/src/web-components/About.js
+++ front/src/web-components/About.js
@@ -10,6 +10,7 @@ import { styled } from '@mui/material/styles';
 import Paper from '@mui/material/Paper';
 import Avatar from '@mui/material/Avatar';
 
+
 const About = () => {
     const bull = (
         <Box
@@ -55,28 +56,23 @@ const About = () => {
         backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
         ...theme.typography.body2,
         padding: theme.spacing(2),
-        maxWidth: 400,
+        maxWidth: 800,
         color: theme.palette.text.primary,
       }));
 
 
-      const message = `Truncation should be conditionally applicable on this long line of text
- as this is a much longer line than what the container can support. `;
+      const message = `About Lorem Ipsum `;
 
     return ( 
         <>
-   <Box sx={{ minWidth: 275,color:'black' ,marginLeft:'300px',width:'900px',height:'500px'}}>
-        <Card sx={{color:'red' ,backgroundColor:'white',marginLeft:'300px',width:'600px',height:'300px'}}
-         variant="outlined">{card}</Card>
+   
 
-<Grid container spacing={2} columns={16} sx={{height:'90px'}}>
+{/* <Grid container spacing={2} columns={16} sx={{height:'90px'}}>
   <Grid item xs={15} sx={{height:'90px'}}>
     <Item> <Typography noWrap>{message}</Typography></Item>
   </Grid>
-  <Grid item xs={8}>
-    <Item>xs=8</Item>
-  </Grid>
-</Grid>
+ 
+</Grid> */}
 
 <Box sx={{ flexGrow: 1, overflow: 'hidden', px: 3 }}>
       <StyledPaper
@@ -87,48 +83,79 @@ const About = () => {
         }}
       >
         <Grid container wrap="nowrap" spacing={2}>
-          <Grid item>
-            <Avatar>W</Avatar>
-          </Grid>
-          <Grid item xs zeroMinWidth>
-            <Typography noWrap>{message}</Typography>
-          </Grid>
-        </Grid>
+
+      
+         
+         <Grid item xs>
+           <Typography>{message}</Typography>
+         </Grid>
+       </Grid>
+
+       <Box
+      sx={{
+        width: 300,
+        height: 300,
+        backgroundColor: 'primary.dark',
+        '&:hover': {
+          backgroundColor: 'primary.main',
+          opacity: [0.9, 0.8, 0.7],
+        },
+      }}
+    >{message}</Box>
+
+<Box
+      sx={{
+        width: 300,
+        height: 300,
+        backgroundColor: 'primary.dark',
+        '&:hover': {
+          backgroundColor: 'primary.main',
+          opacity: [0.9, 0.8, 0.7],
+        },
+      }}
+    >
+
+Blockchain for business uses a shared and immutable ledger that can only be accessed by members with permission. Network members control what information each organization or member may see, and what actions each can take. Blockchain is sometimes called a “trustless” network — not because business partners don’t trust each other, but because they don’t have to.
+
+This trust is built on blockchain’s enhanced security, greater transparency, and instant traceability. Beyond matters of trust, blockchain delivers even more business benefits, including the cost savings from increased speed, efficiency, and automation. By greatly reducing paperwork and errors, blockchain significantly reduces overhead and transaction costs, and reduces or eliminates the need for third parties or middlemen to verify transactions.
+</Box>
+
+        <Box sx={{ minWidth: 275,color:'black' ,marginLeft:'300px',width:'900px',height:'500px'}}>
+        <Card sx={{color:'red', marginTop:'150px',backgroundColor:'white',marginLeft:'-300px',width:'500px',height:'300px'}}
+         variant="outlined">{card}</Card>
+
+
+
+
+
+
+
+
+
+         </Box>
+        
       </StyledPaper>
+     
+      
+     
       <StyledPaper
         sx={{
           my: 1,
           mx: 'auto',
           p: 2,
+          backgroundColor:'black'
+
         }}
       >
         <Grid container wrap="nowrap" spacing={2}>
-          <Grid item>
-            <Avatar>W</Avatar>
-          </Grid>
-          <Grid item xs>
-            <Typography noWrap>{message}</Typography>
-          </Grid>
-        </Grid>
-      </StyledPaper>
-      <StyledPaper
-        sx={{
-          my: 1,
-          mx: 'auto',
-          p: 2,
-        }}
-      >
-        <Grid container wrap="nowrap" spacing={2}>
-          <Grid item>
-            <Avatar>W</Avatar>
-          </Grid>
-          <Grid item xs>
+         
+          <Grid item xs >
             <Typography>{message}</Typography>
           </Grid>
         </Grid>
       </StyledPaper>
     </Box>
-      </Box>
+   
 
 <div style={{display:'flex',justifyContent:'center'}}>
 <Card sx={{ minWidth: 275, backgroundColor:'black',marginTop:'20px', width:'300px', marginLeft:'50px' }}>
