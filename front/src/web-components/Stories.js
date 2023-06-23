6cc9f041bc89bc99773ff2ec4d03cec394a7c52b
Habte Selassie <habtuchelsea1000@gmail.com>
2023-06-23 17:00:47 +0300

    update commit

--- front/src/web-components/Stories.js
+++ front/src/web-components/Stories.js
@@ -9,7 +9,8 @@ import AddIcon from '@mui/icons-material/AddCircleRounded';
 
 export default function Stories() {
   return (
-    <Card sx={{ maxWidth: 345 }}>
+    <div style={{display:'flex',justifyContent:'center'}}>
+<Card sx={{ maxWidth: 345,marginLeft:'10px'}}>
       <CardActionArea>
         <CardMedia
           component="img"
@@ -29,9 +30,72 @@ export default function Stories() {
       </CardActionArea>
       <CardActions>
         <Button size="small" color="primary">
-         <AddIcon/>
+         <AddIcon style={{ fontSize: '2.6rem' }}/>
         </Button>
       </CardActions>
     </Card>
+
+    <Card sx={{ maxWidth: 345,marginLeft:'10px',  }}>
+      <CardActionArea>
+        <CardMedia
+          component="img"
+          height="140"
+          image="/static/images/cards/contemplative-reptile.jpg"
+          alt="green iguana"
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="div">
+            Lizard
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+            Lizards are a widespread group of squamate reptiles, with over 6,000
+            species, ranging across all continents except Antarctica
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+      <CardActions>
+        <Button size="small" color="primary">
+         <AddIcon style={{ fontSize: '2.6rem' }} />
+        </Button>
+      </CardActions>
+    </Card>
+
+
+    <Card sx={{ maxWidth: 345, marginLeft:'10px',  }}>
+      <CardActionArea>
+        <CardMedia
+          component="img"
+          height="140"
+          image="/static/images/cards/contemplative-reptile.jpg"
+          alt="green iguana"
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="div">
+            Lizard
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+            Lizards are a widespread group of squamate reptiles, with over 6,000
+            species, ranging across all continents except Antarctica
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+      <CardActions>
+        <Button size="small" color="primary">
+         <AddIcon style={{ fontSize: '2.6rem' }}/>
+        </Button>
+      </CardActions>
+    </Card>
+
+
+    <button
+        style={{ width: '150px', height: '60px', backgroundColor: 'green', borderRadius: '15px',marginTop:'100px' }}
+      >
+        Lorem Ipsum
+      </button>
+
+
+
+    </div>
+    
   );
 }
