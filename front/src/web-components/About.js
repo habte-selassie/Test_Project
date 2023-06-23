3fb75dec7f77c0e45d92c236872c049913586d7a
Habte Selassie <habtuchelsea1000@gmail.com>
2023-06-23 14:05:02 +0300

    add grid,card and box to about component

--- front/src/web-components/About.js
+++ front/src/web-components/About.js
@@ -5,11 +5,14 @@ import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
+import Grid from '@mui/material/Grid'
+import { styled } from '@mui/material/styles';
+import Paper from '@mui/material/Paper';
 const About = () => {
     const bull = (
         <Box
           component="span"
-          sx={{ display: 'inline-block', mx: '2px', transform: 'scale(0.8)' }}
+          sx={{ color:'black',display: 'inline-block', mx: '2px', transform: 'scale(0.8)' }}
         >
           •
         </Box>
@@ -37,13 +40,32 @@ const About = () => {
           </CardActions>
         </React.Fragment>
       );
+
+      const Item = styled(Paper)(({ theme }) => ({
+        backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+        ...theme.typography.body2,
+        padding: theme.spacing(1),
+        textAlign: 'center',
+        color: theme.palette.text.secondary,
+      }));
     return ( 
         <>
-   <Box sx={{ minWidth: 275,color:'white' ,marginLeft:'300px',width:'600px',height:'300px'}}>
-        <Card sx={{color:'red' ,backgroundColor:'white'}} variant="outlined">{card}</Card>
+   <Box sx={{ minWidth: 275,color:'black' ,marginLeft:'300px',width:'900px',height:'500px'}}>
+        <Card sx={{color:'red' ,backgroundColor:'white',marginLeft:'300px',width:'600px',height:'300px'}}
+         variant="outlined">{card}</Card>
+
+<Grid container spacing={2} columns={16}>
+  <Grid item xs={8}>
+    <Item>xs=8</Item>
+  </Grid>
+  <Grid item xs={8}>
+    <Item>xs=8</Item>
+  </Grid>
+</Grid>
       </Box>
 
-      <Card sx={{ minWidth: 275 }}>
+<div style={{display:'flex',justifyContent:'center'}}>
+<Card sx={{ minWidth: 275, backgroundColor:'black',marginTop:'20px', width:'300px', marginLeft:'50px' }}>
       <CardContent>
         <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
           Word of the Day
@@ -65,7 +87,7 @@ const About = () => {
       </CardActions>
     </Card>
 
-    <Card sx={{ minWidth: 275,color:'red' ,backgroundColor:'black'}}>
+    <Card sx={{ minWidth: 275,color:'red' ,backgroundColor:'black',marginTop:'20px', width:'300px', marginLeft:'50px'}}>
       <CardContent>
         <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
           Word of the Day
@@ -87,7 +109,7 @@ const About = () => {
       </CardActions>
     </Card>
 
-    <Card sx={{ minWidth: 275 }}>
+    <Card sx={{ minWidth: 275, backgroundColor:'black', marginTop:'20px', width:'300px', marginLeft:'50px' }}>
       <CardContent>
         <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
           Word of the Day
@@ -108,6 +130,8 @@ const About = () => {
         <Button size="small">Learn More</Button>
       </CardActions>
     </Card>
+</div>
+      
         </>
      
      );
