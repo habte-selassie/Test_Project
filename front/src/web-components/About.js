0f13c164e60207e9809fab2778ec31ce6583cf4b
Habte Selassie <habtuchelsea1000@gmail.com>
2023-06-23 14:12:35 +0300

    update commit

--- front/src/web-components/About.js
+++ front/src/web-components/About.js
@@ -8,6 +8,8 @@ import Typography from '@mui/material/Typography';
 import Grid from '@mui/material/Grid'
 import { styled } from '@mui/material/styles';
 import Paper from '@mui/material/Paper';
+import Avatar from '@mui/material/Avatar';
+
 const About = () => {
     const bull = (
         <Box
@@ -48,20 +50,84 @@ const About = () => {
         textAlign: 'center',
         color: theme.palette.text.secondary,
       }));
+
+      const StyledPaper = styled(Paper)(({ theme }) => ({
+        backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+        ...theme.typography.body2,
+        padding: theme.spacing(2),
+        maxWidth: 400,
+        color: theme.palette.text.primary,
+      }));
+
+
+      const message = `Truncation should be conditionally applicable on this long line of text
+ as this is a much longer line than what the container can support. `;
+
     return ( 
         <>
    <Box sx={{ minWidth: 275,color:'black' ,marginLeft:'300px',width:'900px',height:'500px'}}>
         <Card sx={{color:'red' ,backgroundColor:'white',marginLeft:'300px',width:'600px',height:'300px'}}
          variant="outlined">{card}</Card>
 
-<Grid container spacing={2} columns={16}>
-  <Grid item xs={8}>
-    <Item>xs=8</Item>
+<Grid container spacing={2} columns={16} sx={{height:'90px'}}>
+  <Grid item xs={15} sx={{height:'90px'}}>
+    <Item> <Typography noWrap>{message}</Typography></Item>
   </Grid>
   <Grid item xs={8}>
     <Item>xs=8</Item>
   </Grid>
 </Grid>
+
+<Box sx={{ flexGrow: 1, overflow: 'hidden', px: 3 }}>
+      <StyledPaper
+        sx={{
+          my: 1,
+          mx: 'auto',
+          p: 2,
+        }}
+      >
+        <Grid container wrap="nowrap" spacing={2}>
+          <Grid item>
+            <Avatar>W</Avatar>
+          </Grid>
+          <Grid item xs zeroMinWidth>
+            <Typography noWrap>{message}</Typography>
+          </Grid>
+        </Grid>
+      </StyledPaper>
+      <StyledPaper
+        sx={{
+          my: 1,
+          mx: 'auto',
+          p: 2,
+        }}
+      >
+        <Grid container wrap="nowrap" spacing={2}>
+          <Grid item>
+            <Avatar>W</Avatar>
+          </Grid>
+          <Grid item xs>
+            <Typography noWrap>{message}</Typography>
+          </Grid>
+        </Grid>
+      </StyledPaper>
+      <StyledPaper
+        sx={{
+          my: 1,
+          mx: 'auto',
+          p: 2,
+        }}
+      >
+        <Grid container wrap="nowrap" spacing={2}>
+          <Grid item>
+            <Avatar>W</Avatar>
+          </Grid>
+          <Grid item xs>
+            <Typography>{message}</Typography>
+          </Grid>
+        </Grid>
+      </StyledPaper>
+    </Box>
       </Box>
 
 <div style={{display:'flex',justifyContent:'center'}}>
