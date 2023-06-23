b09bc1b402bc4ad271201d4c2f431e1a40ec5d69
Habte Selassie <habtuchelsea1000@gmail.com>
2023-06-23 15:00:34 +0300

    update commit

--- front/src/web-components/Body.js
+++ front/src/web-components/Body.js
@@ -1,5 +1,7 @@
+import React from 'react'
 import Box from '@mui/material/Box';
 import Paper from '@mui/material/Paper';
+import Button from '@mui/material/Button'
 
 const Body = () => {
     return ( 
@@ -9,13 +11,27 @@ const Body = () => {
         display: 'flex',
         '& > :not(style)': {
           m: 1,
-          width: 128,
-          height: 128,
+          width: 328,
+          height: 328,
         },
       }}
     >
-      <Paper variant="outlined" />
-      <Paper variant="outlined" square />
+    
+      <Paper variant="outlined">
+      <h1> Lorem ipsum dolor sit consectetur.</h1>
+
+      <p>Mauris suscipit faucibus felis risus lectus platea neque ut.
+       Odio adipiscing tincidunt velit egestas convallis placerat vestibulum. </p>
+      <Button variant="contained">Contained</Button>
+      </Paper>
+     
+      <Paper variant="outlined" square >
+      <h1> Lorem ipsum dolor sit consectetur.</h1>
+
+<p>Mauris suscipit faucibus felis risus lectus platea neque ut.
+ Odio adipiscing tincidunt velit egestas convallis placerat vestibulum. </p>
+<Button variant="contained">Contained</Button>
+      </Paper>
     </Box>
         </div>
      );
