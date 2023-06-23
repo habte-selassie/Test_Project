3d9f5a7caee4b0e014d6efb7005475d439c813c4
Habte Selassie <habtuchelsea1000@gmail.com>
2023-06-23 14:50:27 +0300

    add jsx code to Body Components

--- front/src/index.js
+++ front/src/index.js
@@ -6,13 +6,15 @@ import "bootstrap/dist/css/bootstrap.css"
 import "font-awesome/css/font-awesome.css"
 import Navigation from "./web-components/Navigation";
 import About from "./web-components/About";
+import Body from "./web-components/Body";
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
   {/* <Navigation/> */}
-  <About/>
+  {/* <About/> */}
+  <Body/>
 </React.StrictMode>
 );
 
--- front/src/web-components/Body.js
+++ front/src/web-components/Body.js
@@ -1,1 +1,24 @@
-sfc
+import Box from '@mui/material/Box';
+import Paper from '@mui/material/Paper';
+
+const Body = () => {
+    return ( 
+        <div>
+ <Box
+      sx={{
+        display: 'flex',
+        '& > :not(style)': {
+          m: 1,
+          width: 128,
+          height: 128,
+        },
+      }}
+    >
+      <Paper variant="outlined" />
+      <Paper variant="outlined" square />
+    </Box>
+        </div>
+     );
+}
+ 
+export default Body;
