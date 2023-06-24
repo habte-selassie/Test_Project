2ae7ca05315e2faf93f1d9946c8b94cf8f93f385
Habte Selassie <habtuchelsea1000@gmail.com>
2023-06-24 06:43:29 +0300

    finish body component

--- front/src/web-components/Body.js
+++ front/src/web-components/Body.js
@@ -5,8 +5,8 @@ import Button from '@mui/material/Button'
 
 const Body = () => {
     return ( 
-        <div style={{backgroundColor:'grey'}}>
-        <img src={require('.././web-components/assets/border-1.png')} alt=''/>
+        <div style={{backgroundColor:'grey',height:'600px'}}>
+        <img  style={{width:'100%',height:'210px',marginTop:'-60px'}} src={require('.././web-components/assets/border-1.png')} alt=''/>
  <Box
       sx={{
         display: 'flex',      
@@ -19,7 +19,7 @@ const Body = () => {
       }}
     >
     
-    <div style={{marginLeft:'150px',width:'390px',height:'500px',marginTop:'200px',backgroundColor:'grey'}}>
+    <div style={{marginLeft:'150px',width:'390px',height:'500px',marginTop:'100px',backgroundColor:'grey'}}>
     <Paper variant="outlined" style={{marginLeft:'10px',width:'500px',height:'300px',border:'1px solid grey',backgroundColor:'gray'}}>
       <h3> Lorem ipsum dolor sit consectetur.</h3>
 
@@ -29,18 +29,20 @@ const Body = () => {
       </Paper>
     </div>
      
-    <div style={{marginLeft:'100px',width:'390px',height:'500px',marginTop:'200px',backgroundColor:'grey'}}>
-    <Paper variant="outlined" style={{marginLeft:'60px',width:'500px',height:'300px',border:'1px solid grey',backgroundColor:'gray'}}>
+    <div style={{marginLeft:'100px',width:'390px',marginTop:'100px',backgroundColor:'grey'}}>
+    <Paper variant="outlined" style={{backgroundColor:'grey',marginLeft:'60px',width:'500px',height:'300px',border:'1px solid grey',backgroundColor:'gray'}}>
       <h3> Lorem ipsum dolor sit consectetur.</h3>
 
       <p style={{marginTop:'30px'}}>Mauris suscipit faucibus felis risus lectus platea neque ut.
        Odio adipiscing tincidunt velit egestas convallis placerat vestibulum. </p>
       <Button variant="contained"  sx={{backgroundColor:'black',width:'200px', marginTop:'100px',height:'60px',borderRadius:'29px'}}>lorem ipsum</Button>
       </Paper>
+      <img style={{marginLeft:'-650px',backgroundColor:'grey',width:'1300px',height:'210px',marginTop:'-10px'}} src={require('.././web-components/assets/border-1.png')} alt=''/> 
+
     </div>
       
     </Box>
-    <img src={require('.././web-components/assets/border-1.png')} alt=''/>
+    {/* <img style={{width:'100%',height:'210px',marginTop:'-30px'}} src={require('.././web-components/assets/border-1.png')} alt=''/> */}
         </div>
      );
 }
