de5c4d3272dc7d852a3b96eb14264c72e3af0e82
Habte Selassie <habtuchelsea1000@gmail.com>
2023-06-24 07:41:27 +0300

    finshed about section

--- front/src/index.js
+++ front/src/index.js
@@ -17,12 +17,12 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
   {/* <Navigation/> */}
-  {/* <About/> */}
+  <About/>
   {/* <Body/> */}
   {/* <Stories/> */}
   {/* <Footer/> */}
   {/* <PrivacyPolicy /> */}
-  <Advertisment/>
+  {/* <Advertisment/> */}
 </React.StrictMode>
 );
 
--- front/src/web-components/About.js
+++ front/src/web-components/About.js
@@ -60,19 +60,17 @@ const About = () => {
         color: theme.palette.text.primary,
       }));
 
+const passage = `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer cursus, felis at consectetur malesuada, odio erat volutpat enim, nec feugiat velit justo vel neque. Nulla dictum quam eu lacus tincidunt ultrices. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Mauris nec tellus eget mauris consequat lacinia. Nulla posuere nibh sit amet justo lacinia, eget aliquet elit vestibulum. Fusce suscipit ligula et dui varius varius. Sed ac dui faucibus, mollis erat nec, facilisis lectus. Sed lobortis consectetur quam, id pharetra dui malesuada non. Donec et nunc non massa iaculis suscipit ut ut lorem. Curabitur sit amet fermentum lectus.
 
-      const message = `About Lorem Ipsum `;
+Vestibulum et lobortis elit, sed bibendum turpis. Morbi pellentesque est quis dui dignissim, nec convallis risus ultrices. `
+      const message = `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer cursus, felis at consectetur malesuada, odio erat volutpat enim, nec feugiat velit justo vel neque. Nulla dictum quam eu lacus tincidunt ultrices. Pellentesque habitant morbi tristique
+       senectus et netus et malesuada fames ac turpis egestas `;
 
     return ( 
         <>
    
 
-{/* <Grid container spacing={2} columns={16} sx={{height:'90px'}}>
-  <Grid item xs={15} sx={{height:'90px'}}>
-    <Item> <Typography noWrap>{message}</Typography></Item>
-  </Grid>
- 
-</Grid> */}
+
 
 <Box sx={{ flexGrow: 1, overflow: 'hidden', px: 3 }}>
       <StyledPaper
@@ -80,6 +78,8 @@ const About = () => {
           my: 1,
           mx: 'auto',
           p: 2,
+          height:'350px',
+          marginTop:'30px'
         }}
       >
         <Grid container wrap="nowrap" spacing={2}>
@@ -87,11 +87,27 @@ const About = () => {
       
          
          <Grid item xs>
-           <Typography>{message}</Typography>
+         <Typography variant="h5"
+         component="div"
+         sx={{
+        textDecoration: 'underline',
+        textDecorationColor: 'yellowgreen',
+        textDecorationStyle: 'solid',
+        fontWeight: 'bold',
+        color: 'yellowgreen',
+      }}>
+        About Lorem Ipsum
+        </Typography>       
+        <Typography variant="body2"
+        sx={{marginTop:'30px'}}>
+         {passage}
+          <br />
+          {'"a benevolent smile"'}
+        </Typography>
          </Grid>
        </Grid>
 
-       <Box
+       {/* <Box
       sx={{
         width: 300,
         height: 300,
@@ -101,9 +117,9 @@ const About = () => {
           opacity: [0.9, 0.8, 0.7],
         },
       }}
-    >{message}</Box>
+    >{message}</Box> */}
 
-<Box
+{/* <Box
       sx={{
         width: 300,
         height: 300,
@@ -118,111 +134,87 @@ const About = () => {
 Blockchain for business uses a shared and immutable ledger that can only be accessed by members with permission. Network members control what information each organization or member may see, and what actions each can take. Blockchain is sometimes called a “trustless” network — not because business partners don’t trust each other, but because they don’t have to.
 
 This trust is built on blockchain’s enhanced security, greater transparency, and instant traceability. Beyond matters of trust, blockchain delivers even more business benefits, including the cost savings from increased speed, efficiency, and automation. By greatly reducing paperwork and errors, blockchain significantly reduces overhead and transaction costs, and reduces or eliminates the need for third parties or middlemen to verify transactions.
-</Box>
+</Box> */}
 
-        <Box sx={{ minWidth: 275,color:'black' ,marginLeft:'300px',width:'900px',height:'500px'}}>
+        {/* <Box sx={{ minWidth: 275,color:'black' ,marginLeft:'300px',width:'900px',height:'500px'}}>
         <Card sx={{color:'red', marginTop:'150px',backgroundColor:'white',marginLeft:'-300px',width:'500px',height:'300px'}}
          variant="outlined">{card}</Card>
-
-
-
-
-
-
-
-
-
-         </Box>
+         </Box> */}
         
       </StyledPaper>
      
-      
-     
-      <StyledPaper
-        sx={{
-          my: 1,
-          mx: 'auto',
-          p: 2,
-          backgroundColor:'black'
-
-        }}
-      >
-        <Grid container wrap="nowrap" spacing={2}>
-         
-          <Grid item xs >
-            <Typography>{message}</Typography>
-          </Grid>
-        </Grid>
-      </StyledPaper>
+    
     </Box>
    
 
 <div style={{display:'flex',justifyContent:'center'}}>
-<Card sx={{ minWidth: 275, backgroundColor:'black',marginTop:'20px', width:'300px', marginLeft:'50px' }}>
-      <CardContent>
-        <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
-          Word of the Day
-        </Typography>
-        <Typography variant="h5" component="div">
-          be{bull}nev{bull}o{bull}lent
-        </Typography>
-        <Typography sx={{ mb: 1.5 }} color="text.secondary">
-          adjective
-        </Typography>
+
+    <Card sx={{ minWidth: 275, backgroundColor:'white', marginTop:'20px', width:'300px', marginLeft:'50px' }}>
+      <CardContent>      
+        <Typography variant="h5"
+         component="div"
+         sx={{
+        textDecoration: 'underline',
+        textDecorationColor: 'yellowgreen',
+        textDecorationStyle: 'solid',
+        fontWeight: 'bold',
+        color: 'yellowgreen',
+      }}>
+         Mission
+        </Typography>       
         <Typography variant="body2">
-          well meaning and kindly.
+         {message}
           <br />
           {'"a benevolent smile"'}
         </Typography>
-      </CardContent>
-      <CardActions>
-        <Button size="small">Learn More</Button>
-      </CardActions>
+      </CardContent>     
     </Card>
 
-    <Card sx={{ minWidth: 275,color:'red' ,backgroundColor:'black',marginTop:'20px', width:'300px', marginLeft:'50px'}}>
-      <CardContent>
-        <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
-          Word of the Day
-        </Typography>
-        <Typography variant="h5" component="div">
-          be{bull}nev{bull}o{bull}lent
-        </Typography>
-        <Typography sx={{ mb: 1.5 }} color="text.secondary">
-          adjective
-        </Typography>
+    <Card sx={{ minWidth: 275, backgroundColor:'white', marginTop:'20px', width:'300px', marginLeft:'50px' }}>
+      <CardContent>      
+        <Typography variant="h5"
+         component="div"
+         sx={{
+        textDecoration: 'underline',
+        textDecorationColor: 'yellowgreen',
+        textDecorationStyle: 'solid',
+        fontWeight: 'bold',
+        color: 'yellowgreen',
+      }}>
+         Vision
+        </Typography>       
         <Typography variant="body2">
-          well meaning and kindly.
+         {message}
           <br />
           {'"a benevolent smile"'}
         </Typography>
-      </CardContent>
-      <CardActions>
-        <Button size="small">Learn More</Button>
-      </CardActions>
+      </CardContent>     
     </Card>
 
-    <Card sx={{ minWidth: 275, backgroundColor:'black', marginTop:'20px', width:'300px', marginLeft:'50px' }}>
-      <CardContent>
-        <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
-          Word of the Day
-        </Typography>
-        <Typography variant="h5" component="div">
-          be{bull}nev{bull}o{bull}lent
-        </Typography>
-        <Typography sx={{ mb: 1.5 }} color="text.secondary">
-          adjective
-        </Typography>
+  <Card sx={{ minWidth: 275, backgroundColor:'white', marginTop:'20px', width:'300px', marginLeft:'50px' }}>
+      <CardContent>      
+        <Typography variant="h5"
+         component="div"
+         sx={{
+        textDecoration: 'underline',
+        textDecorationColor: 'yellowgreen',
+        textDecorationStyle: 'solid',
+        fontWeight: 'bold',
+        color: 'yellowgreen',
+      }}>
+         Objectives
+        </Typography>       
         <Typography variant="body2">
-          well meaning and kindly.
+         {message}
           <br />
           {'"a benevolent smile"'}
         </Typography>
-      </CardContent>
-      <CardActions>
-        <Button size="small">Learn More</Button>
-      </CardActions>
+      </CardContent>     
     </Card>
+
+    
+
+    
 </div>
       
         </>
